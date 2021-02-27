384800b9058a8c2e89da117e22f392463842a3fb
Adaypon <75831434+Adaypon@users.noreply.github.com>
2021-02-27 21:48:35 +0300

    Update MyString.cpp
    - added constructor with rawString
    - copy constructor
    - move constructor
    - copy assignment operator
    - added append function
    - added insert function

--- MyString.cpp
+++ MyString.cpp
@@ -4,26 +4,81 @@
 #include <cstdlib>
 #include <cstring>
 
-MyString::MyString(const char* rawString) {
-    // TODO
+MyString::MyString(const char* rawString) 
+	: _size(strlen(rawString)), _data(new char[_size]) {
+	//| TODO try memcpy()
+    for (size_t i = 0; i < _size; ++i) {
+		this->_data[i] = rawString[i];		
+	}
 }
 
 MyString::MyString(const MyString& other) {
-    // TODO
+	this->_size = other._size;
+	this->_data = new char[_size];
+	for (size_t i = 0; i < this->_size; ++i) {
+		this->_data[i] = copy._data[i];
+	}
 }
 
-MyString::MyString(MyString&& other) {
-
+MyString::MyString(MyString&& other) noexcept {
+	this->_size = other._size;
+	this->_data = ohter._data;
+	other._size = 0;
+	other._data = nullptr;
 }
 
 MyString& MyString::operator=(const MyString& other) {
-    // TODO
+	MyString copy(other);
+	std::swap(this->_data, copy._data);
+	std::swap(this->_size, copy._size);
     return *this;
 }
 
-MyString& MyString::operator=(MyString&& other) {
+MyString& MyString::operator=(MyString&& other) noexcept {
     // TODO
+	
     return *this;
+}
+
+MyString::~MyString() {
+	delete[] _data;
+}
+
+void MyString::append(const MyString& appendedString) {
+	char* tmpString = new char[this->_size + appendedString._size];
+	for (size_t i = 0; i < this->_size; ++i) {
+		tmpString[i] = this->_data;
+	}
+	for (size_t j = 0; j < appendedString._size; ++j) {
+		tmpString[j] = appendedString._data[j];		
+	}
+	delete[] this->_data;
+	this->_data = tmpString;
+	this->_size += appendedString;
+}
+
+void MyString::insert(unsigned int pos, const MyString& insertedString) {
+	if (pos >= this->_size) {
+		// non-valid pos
+		return;
+	}
+	size_t i = 0, j = 0;
+	unsigned int tmpSize = this->_size + insertedString._size;
+	char* tmpString = new char[tmpSize];
+	
+	
+	for (; i < pos; ++i) {
+		tmpString[i] = this->_data[i];
+	}
+	for (j = i; j < insertedString._size; ++j) {
+		tmpString[j] = insertedString._data[j];
+	}
+	for (; j < tmpSize; j++, i++) {
+		tmpString[j] = this->_data[i];
+	}
+	delete[] this->_data;
+	this->_data = tmpString;
+	this->_size = tmpSize;
 }
 
 char& MyString::at(const unsigned int idx) {
@@ -40,7 +95,7 @@ char& MyString::operator[](const unsigned int idx) {
     return at(idx);
 }
 
-char& MyString::operator[](const unsigned int idx) const {
+const char& MyString::operator[](const unsigned int idx) const {
     return at(idx);
 }
 
